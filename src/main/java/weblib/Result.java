bf23e4496eac64a21e7e8ca5e61d2464d7b63ff5
pantellerija <polina.pavlova19.10@gmail.com>
2022-02-10 23:39:34 +0300

    configs added

--- src/main/java/weblib/Result.java
+++ src/main/java/weblib/Result.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Data
+//@Data
 public class Result {
     double x;
     double y;
@@ -23,23 +23,51 @@ public class Result {
         currentTime = LocalDateTime.now().format(formatter);
     }
 
-
     private boolean checkQuarters() {
-        return checkFirstQuarter() || checkSecondQuarter() || checkFourthQuarter();
+        return x <= 0 && y >= 0 && x * x + y * y <= r / 2
+                || x >= 0 && y >= 0 && x <= r / 2 && y <= r
+                || x >= 0 && y <= 0 && y >= x - r / 2;
     }
 
-    private boolean checkFirstQuarter() {
-        return x <= 0 && y >= 0 && x * x + y * y <= r / 2;
+    public double getX() {
+        return x;
     }
 
-    private boolean checkSecondQuarter() {
-        return x >= 0 && y >= 0 && x <= r / 2 && y <= r;
+    public void setX(double x) {
+        this.x = x;
     }
 
-    private boolean checkFourthQuarter() {
-        return x >= 0 && y <= 0 && y >= x - r / 2;
+    public double getY() {
+        return y;
     }
 
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
 }
 
 
--- src/main/java/weblib/ResultBean.java
+++ src/main/java/weblib/ResultBean.java
@@ -5,13 +5,31 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+//@Data
 public class ResultBean {
     private Result newResult = new Result();
     private List<Result> results = new ArrayList<>();
 
     public void addResult() {
+        newResult.generateStatus();
+        newResult.generateTime();
         results.add(newResult);
         newResult = new Result();
+    }
+
+    public Result getNewResult() {
+        return newResult;
+    }
+
+    public void setNewResult(Result newResult) {
+        this.newResult = newResult;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
     }
 }
